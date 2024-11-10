44788a0546a5d0356608a219a86061cf39a1df4c
unknown <azzurre123@gmail.com>
2024-11-10 20:41:36 +0800

    refined viz

--- gym-multigrid-master/visualize_stats.py
+++ gym-multigrid-master/visualize_stats.py
@@ -1,14 +1,14 @@
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib
-from config import ENV_CLASS, ENV_RULE_SETS
-from plot import exponential_smoothing
+
 import os
 from os.path import join, dirname
 import pickle
 
 import numpy as np 
 from collections import Counter
+from matplotlib.ticker import MaxNLocator
 
 matplotlib.use("Agg")
 
@@ -20,6 +20,7 @@ parser.add_argument("--model_dir",  default=join(dirname(os.path.abspath(__file_
                     help="model_dir")
 
 args = parser.parse_args()
+args.env = "key"
 
 
 episode_rewards = {}
@@ -37,7 +38,7 @@ for file in os.listdir(PATH):
     else:
         ruleset = ""
     
-    use_rules_str= f", rulesest: {ruleset}" if USE_KG else ""
+    use_rules_str= f", {ruleset}" if USE_KG else ""
     label = f"{model_type}" + use_rules_str
 
     with open(join(PATH,file), "rb") as handle:
@@ -50,71 +51,44 @@ for file in os.listdir(PATH):
 
 #  Plot 
 
-colors = plt.get_cmap("tab20").colors  
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"]
 
-# fig, ax = plt.subplots(figsize=(10, 6))
-# for c,(label, data) in enumerate(episode_rewards.items()):
-#     # Create plot
-
-#     # Compute and plot smoothed values
-#     ma = exponential_smoothing(data, alpha=0.3)
-#     ma_idx_start = len(data) - len(ma)
-#     ax.plot(range(ma_idx_start, len(data)), ma, label=label,
-#             linestyle="-", color=colors[c], linewidth=2)
-
-#     # # Shade the area under the original data
-#     # ax.fill_between(range(len(data)), data, color=colors[c], alpha=0.1)
-
-# # Add labels and legend
-# ax.set_xlabel("Episode")
-# ax.set_ylabel("Rewards per episodes")
-# ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
-# plt.savefig(f"{args.env}_episode_rewards_plot.png", bbox_inches='tight') # Save the
-# plt.close()
-
-# Define the desired legend order
-desired_legend_order = ['COMA', 'COMA, rulesest: ball only', 'COMA, rulesest: ball with search strats', 'COMA, rulesest: conflicting rules', 'COMA, rulesest: irrelevant rules', 
-                        'COMA, rulesest: lava + ball',
-                        'COMA, rulesest: ball + key',
-                        'MAPPO', 'MAPPO, rulesest: ball only', 'MAPPO, rulesest: ball with search strats', 'MAPPO, rulesest: conflicting rules', 'MAPPO, rulesest: irrelevant rules',  
-                        'MAPPO, rulesest: lava + ball', 
-                        'MAPPO, rulesest: ball + key',
-                        'random',
-                        'expert, rulesest: ball only', 'expert, rulesest: ball with search strats', 'expert, rulesest: conflicting rules', 'expert, rulesest: irrelevant rules',
-                        'expert, rulesest: lava + ball',
-                        'expert, rulesest: ball + key']  
-
-
-fig, ax = plt.subplots(figsize=(10, 6))
-lines = []  # To store line objects
-labels = []  # To store corresponding labels
-
-for c, (label, data) in enumerate(episode_rewards.items()):
-    # Compute and plot smoothed values
-    ma = exponential_smoothing(data, alpha=0.3)
-    ma_idx_start = len(data) - len(ma)
-    line, = ax.plot(range(ma_idx_start, len(data)), ma, label=label,
-                    linestyle="-", color=colors[c], linewidth=2)
+sublabel = ["COMA", "MAPPO", "others"]
+for s in sublabel:
+    fig, ax = plt.subplots(figsize=(10, 6))
     
-    # Store line and label only if it's in the desired legend order
-    if label in desired_legend_order:
-        lines.append(line)
-        labels.append(label)
+    
+    if s == "COMA" or s == "MAPPO":
+        data_dict ={ x:y for x,y in episode_rewards.items() if s in x}
+    else:
+        
+        data_dict ={ x:y for x,y in episode_rewards.items() if "expert" in x or "random" in x}
+    
+    color_map= {label: colors[i] for i, label in enumerate(data_dict)}
+    max_count = 0
+    for c,(label, data) in enumerate(data_dict.items()):
+        # Plot overlapping histograms
+        counts, _, _ =ax.hist(data, bins=50, alpha=0.5, label=label, color=color_map[label])
+        max_count = max(max_count, max(counts))
 
-# Reorder legend dynamically based on existing labels
-ordered_handles = [lines[labels.index(label)] for label in desired_legend_order if label in labels]
-ordered_labels = [label for label in desired_legend_order if label in labels]
 
-ax.set_xlabel("Episode")
-ax.set_ylabel("Rewards per episodes")
+    ax.set_ylim(0, max_count)  # Set the y-axis limit to max episodes
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    # Add labels and legend
 
-# Apply reordered legend
-ax.legend(ordered_handles, ordered_labels, loc='upper left', bbox_to_anchor=(1, 1))
+    ax.set_xlabel("Rewards per episode")
+    ax.set_ylabel("Episodes")
+    handles, labels = ax.get_legend_handles_labels()
+    sorted_handles_labels = sorted(zip(labels, handles), key=lambda x: x[0])
+    sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+    ax.legend(sorted_handles, sorted_labels, loc='upper left', bbox_to_anchor=(1, 1))
+    plt.savefig(f"{args.env}_{s}_episode_rewards_plot.png", bbox_inches='tight') # Save the
+    plt.close()
 
-# Save the plot
-plt.savefig(f"{args.env}_episode_rewards_plot.png", bbox_inches='tight')
-plt.close()
 
+
+
+colors = plt.get_cmap("tab20", 15).colors
 # Set up the figure and axis
 fig, ax = plt.subplots(figsize=(10, 6))
 
@@ -125,26 +99,14 @@ y_positions = np.arange(len(categories))
 # Bar height calculation
 bar_height = 0.8 / len(num_agents_died)
 
-# Track handles and labels for legend
-handles = []
-labels = []
-
 # Plotting each rule set
 for c, (label, num_agents_died_arr) in enumerate(num_agents_died.items()):
     # Count occurrences of each category (0, 1, 2, 3, 4 died)
     counts = np.array([Counter(num_agents_died_arr).get(i, 0) for i in range(len(categories))])
     
     # Offset bars for each rule set
-    bar = ax.barh(y_positions - (len(num_agents_died) / 2 - c) * bar_height, counts,
+    ax.barh(y_positions - (len(num_agents_died) / 2 - c) * bar_height, counts,
             height=bar_height, label=label, color=colors[c % len(colors)])
-    
-    if label in desired_legend_order:
-        handles.append(bar)
-   
-        labels.append(label)
-
-ordered_handles = [handles[labels.index(label)] for label in desired_legend_order if label in labels]
-ordered_labels = [label for label in desired_legend_order if label in labels]
 
 # Labels and title
 ax.set_xlabel('Num Episodes')
@@ -153,23 +115,24 @@ ax.set_title('Num Agents Died per Episode')
 ax.set_yticks(y_positions)
 ax.set_yticklabels(categories)
 
-
-ax.legend(ordered_handles, ordered_labels, bbox_to_anchor=(1.05, 1), loc='upper left')
+handles, labels = ax.get_legend_handles_labels()
+sorted_handles_labels = sorted(zip(labels, handles), key=lambda x: x[0])
+sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+ax.legend(sorted_handles, sorted_labels, loc='upper left', bbox_to_anchor=(1.05, 1))
 
 # Show or save the plot
 plt.tight_layout()
 plt.savefig(f"{args.env}_num_agents_died_plot.png")
 plt.close()
 
+
+
+colors = plt.get_cmap("tab20", 15).colors
 # Set up the figure and axis
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Track handles and labels for legend
-handles = []
-labels = []
-
 # Categories and positions
-categories = ['0 collected', '1 ball collected', '2 balls collected', '3 balls collected', '5 balls collected']
+categories = ['0 collected', '1 ball collected', '2 balls collected', '3 balls collected', '4 balls collected' ,'5 balls collected']
 y_positions = np.arange(len(categories))
 
 # Bar height calculation
@@ -181,15 +144,8 @@ for c, (label, num_balls_collected_arr) in enumerate(num_balls_collected.items()
     counts = np.array([Counter(num_balls_collected_arr).get(i, 0) for i in range(len(categories))])
     
     # Offset bars for each rule set
-    bar = ax.barh(y_positions - (len(num_balls_collected) / 2 - c) * bar_height, counts,
+    ax.barh(y_positions - (len(num_balls_collected) / 2 - c) * bar_height, counts,
             height=bar_height, label=label, color=colors[c % len(colors)])
-
-    if label in desired_legend_order:
-        handles.append(bar)
-        labels.append(label)
-
-ordered_handles = [handles[labels.index(label)] for label in desired_legend_order if label in labels]
-ordered_labels = [label for label in desired_legend_order if label in labels]
 
 # Labels and title
 ax.set_xlabel('Num Episodes')
@@ -197,14 +153,16 @@ ax.set_ylabel('Num Balls Collected')
 ax.set_title('Num Balls Collected per Episode')
 ax.set_yticks(y_positions)
 ax.set_yticklabels(categories)
-ax.legend(ordered_handles, ordered_labels, bbox_to_anchor=(1.05, 1), loc='upper left')
+handles, labels = ax.get_legend_handles_labels()
+sorted_handles_labels = sorted(zip(labels, handles), key=lambda x: x[0])
+sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+ax.legend(sorted_handles, sorted_labels, loc='upper left', bbox_to_anchor=(1.05, 1))
 
 # Show or save the plot
 plt.tight_layout()
 plt.savefig(f"{args.env}_num_balls_collected_plot.png")
 plt.close()
 
-from matplotlib.ticker import MaxNLocator
 
 # # Example data
 # wall_hits = {
@@ -217,35 +175,58 @@ from matplotlib.ticker import MaxNLocator
 # }
 
 
-# Calculate the maximum number of episodes (i.e., the max length of any wall_hits list)
-max_episodes = max(len(hits) for hits in wall_hits.values())
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"]
+for s in sublabel:
+    fig, ax = plt.subplots(figsize=(10, 6))
+    
+    if s == "COMA" or s == "MAPPO":
+        data_dict ={ x:y for x,y in wall_hits.items() if s in x}
+    else:
+        
+        data_dict ={ x:y for x,y in wall_hits.items() if "expert" in x or "random" in x}
+    
+    color_map= {label: colors[i] for i, label in enumerate(data_dict)}
+    max_count = 0
+    for c,(label, data) in enumerate(data_dict.items()):
+        # Plot overlapping histograms
+        counts, _, _ =ax.hist(data, bins=10, alpha=0.5, label=label, color=color_map[label])
+        max_count = max(max_count, max(counts))
 
-# Plot overlapping histograms
-plt.figure(figsize=(10, 6))
 
-# Track handles and labels for the legend
-handles = []
-labels = []
+    ax.set_ylim(0, max_count)  # Set the y-axis limit to max episodes
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    # Add labels and legend
 
-for i, (label, data) in enumerate(wall_hits.items()):
-    hist = plt.hist(data, bins=10, alpha=0.5, label=label, color=colors[i % len(colors)])
+    ax.set_xlabel("Wall Hits")
+    ax.set_ylabel("Num of Episodes")
+    handles, labels = ax.get_legend_handles_labels()
+    sorted_handles_labels = sorted(zip(labels, handles), key=lambda x: x[0])
+    sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+    ax.legend(sorted_handles, sorted_labels, loc='upper left', bbox_to_anchor=(1, 1))
+    plt.savefig(f"{args.env}_{s}_wall_hits_plot.png", bbox_inches='tight' )
+    plt.close()
 
-    if label in desired_legend_order:
-        handles.append((hist[2][0], label))
 
-# Reorder legend dynamically based on desired_legend_order
-ordered_handles_labels = [(h, l) for l in desired_legend_order for h, lbl in handles if lbl == l]
-ordered_handles, ordered_labels = zip(*ordered_handles_labels) if ordered_handles_labels else ([], [])
 
-# Set y-axis to use only integer ticks and reflect max episodes
-plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-plt.ylim(0, max_episodes//2)  # Set the y-axis limit to max episodes
 
-# Add labels and legend
-plt.xlabel("Wall Hits")
-plt.ylabel("Num of Episodes")
-plt.title("Wall Hits Episodes")
-plt.legend(ordered_handles, ordered_labels)
 
-# Display the plot
-plt.savefig(f"{args.env}_wall_hits_plot.png")
+# plt.figure(figsize=(10, 6))
+# for i, (label, data) in enumerate(wall_hits.items()):
+#     plt.hist(data, bins=10, alpha=0.5, label=label, color=colors[i % len(colors)])
+
+# # Set y-axis to use only integer ticks and reflect max episodes
+# plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+# plt.ylim(0, max_episodes)  # Set the y-axis limit to max episodes
+
+# # Add labels and legend
+# plt.xlabel("Wall Hits")
+# plt.ylabel("Num of Episodes")
+
+
+# handles, labels = plt.get_legend_handles_labels()
+# sorted_handles_labels = sorted(zip(labels, handles), key=lambda x: x[0])
+# sorted_labels, sorted_handles = zip(*sorted_handles_labels)
+# plt.legend(sorted_handles, sorted_labels, loc='upper left', bbox_to_anchor=(1, 1))
+
+# # Display the plot
+# plt.savefig(f"{args.env}_wall_hits_plot.png", bbox_inches='tight' )
